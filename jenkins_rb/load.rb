151ee79604ccaa7f944f1147980f9a0971594993
Nikolai Colton <ncolton@shoprunner.com>
2014-07-16 16:35:55 -0700

    refactoring jenkins load script

--- jenkins_rb/load.rb
+++ jenkins_rb/load.rb
@@ -4,6 +4,11 @@ require 'sparkr'
 require 'term/ansicolor'
 require 'yaml'
 
+# The magic to make coloring work
+class String
+    include Term::ANSIColor
+end
+
 def load_config(configuration_file)
     fd = open configuration_file, 'r'
     configuration = YAML.load fd.read
@@ -16,11 +21,6 @@ def bucket_resize(data, items_per_bucket)
         buckets << batch.inject(0) { |result, element| result + element }
     end
     return buckets
-end
-
-# The magic to make coloring work
-class String
-    include Term::ANSIColor
 end
 
 def color_sparkline_by_value(data, yellow, red)
@@ -46,39 +46,52 @@ def build_query_url(config)
     "#{scheme}://#{config[:host]}#{config[:base_path]}#{config[:query_path]}?#{config[:query]}"
 end
 
+def write_output(data)
+    line_length = 50
+    puts ''
+    puts '      Executors'
+    puts ' Hourly    |     Total : ' + Sparkr.sparkline(data['executors']['total']['hourly'])
+    puts '           |      Busy : ' + Sparkr.sparkline(data['executors']['busy']['hourly'])
+    puts ' 5 minutes |     Total : ' + Sparkr.sparkline(data['executors']['total']['5min'])
+    puts '           |      Busy : ' + Sparkr.sparkline(data['executors']['busy']['5min'])
+    puts '=' * line_length
+    puts '    Queue  |    Hourly : ' + color_sparkline_by_value(data['queue_length']['hourly'], 5, 10)
+    puts '    Length | 5 minutes : ' + color_sparkline_by_value(data['queue_length']['5min'], 5, 10)
+    puts ''
+end
+
+def parse_json_data(json_data)
+    data = {
+        'executors' => {
+            'total' => {
+                'hourly' => json_data['totalExecutors']['hour']['history'][0..23],
+                '5min' => bucket_resize(json_data['totalExecutors']['min']['history'], 5)[0..11]
+            },
+            'busy' => {
+                'hourly' => json_data['busyExecutors']['hour']['history'][0..23],
+                '5min' => bucket_resize(json_data['busyExecutors']['min']['history'], 5)[0..11]
+            }
+        },
+        'queue_length' => {
+            'hourly' => json_data['totalQueueLength']['hour']['history'][0..23],
+            '5min' => bucket_resize(json_data['totalQueueLength']['min']['history'], 5)[0..11]
+        }
+    }
+
+    return data
+end
+
+def query_jenkins_for_stats(jenkins_configuration)
+    query_url = build_query_url jenkins_configuration
+    # stat_resource = RestClient::Resource.new query_url, :user => auth[:user], :password => auth[:api_token]
+    stat_resource = RestClient::Resource.new query_url
+    stats = JSON.parse(stat_resource.get)
+    return stats
+end
+
+
 default_config_file = '.jenkins_load_config'
 configuration = load_config default_config_file
-query_url = build_query_url configuration[:jenkins]
-
-# stat_resource = RestClient::Resource.new query_url, :user => auth[:user], :password => auth[:api_token]
-stat_resource = RestClient::Resource.new query_url
-stats = JSON.parse(stat_resource.get)
-
-data = {
-    'executors' => {
-        'total' => {
-            'hourly' => stats['totalExecutors']['hour']['history'][0..23],
-            '5min' => bucket_resize(stats['totalExecutors']['min']['history'], 5)[0..11]
-        },
-        'busy' => {
-            'hourly' => stats['busyExecutors']['hour']['history'][0..23],
-            '5min' => bucket_resize(stats['busyExecutors']['min']['history'], 5)[0..11]
-        }
-    },
-    'queue_length' => {
-        'hourly' => stats['totalQueueLength']['hour']['history'][0..23],
-        '5min' => bucket_resize(stats['totalQueueLength']['min']['history'], 5)[0..11]
-    }
-}
-
-line_length = 50
-puts ''
-puts '      Executors'
-puts ' Hourly    |     Total : ' + Sparkr.sparkline(data['executors']['total']['hourly'])
-puts '           |      Busy : ' + Sparkr.sparkline(data['executors']['busy']['hourly'])
-puts ' 5 minutes |     Total : ' + Sparkr.sparkline(data['executors']['total']['5min'])
-puts '           |      Busy : ' + Sparkr.sparkline(data['executors']['busy']['5min'])
-puts '=' * line_length
-puts '    Queue  |    Hourly : ' + color_sparkline_by_value(data['queue_length']['hourly'], 5, 10)
-puts '    Length | 5 minutes : ' + color_sparkline_by_value(data['queue_length']['5min'], 5, 10)
-puts ''
+stats = query_jenkins_for_stats configuration[:jenkins]
+data = parse_json_data(stats)
+write_output(data)
